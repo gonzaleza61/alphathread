40356968dd83c3b0b73efff6f2e7807f1e6850b0
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2024-03-24 23:54:07 -0500

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
+import CardCarousel from "./components/AboutSection";
 
 function App() {
   return (
     <>
       <NavBar />
       <HeroSection />
+      <CardCarousel />
     </>
   );
 }
