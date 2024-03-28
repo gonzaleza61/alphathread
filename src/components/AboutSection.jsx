69a01908df1190add2e27f8232cb3f51691d85f1
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2024-03-28 12:25:33 -0500

    underline

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -7,7 +7,11 @@ export default function AboutUs() {
       <section className="h-screen">
         <div className="flex flex-col m-12 bg-slate-300">
           <div className="flex flex-col m-4 bg-white">
-            <h1 className="text-center">About Us</h1>
+            <h1 className="text-center text-4xl font-bold">
+              <span className="inline-block border-b-2 border-black pb-1">
+                What We Do
+              </span>
+            </h1>
           </div>
           <div className="flex flex-wrap justify-between gap-4 bg-white">
             <Card imgSrc={AboutOneImage} title="center" />
