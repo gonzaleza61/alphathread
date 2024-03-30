89e6bd9eb6c8313be393297c1cfc8cb9771553ff
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2024-03-29 19:22:04 -0500

    flex to grid fix

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,14 +8,14 @@ export default function AboutUs() {
         <div className="flex flex-col m-12 bg-slate-300">
           <div className="flex flex-col m-4 bg-white">
             <h1 className="text-center text-4xl font-bold">
-              <span className="inline-block border-b-2 border-black pb-1">
+              <span className="inline-block border-b-2 border-red-500 pb-1">
                 What We Do
               </span>
             </h1>
           </div>
-          <div className="flex flex-wrap justify-between gap-4 bg-white">
+          <div className="grid grid-rows-4 grid-cols-1 justify-between gap-4 bg-red-500 lg:grid-cols-4 lg:grid-rows-1 sm:grid-rows-2 sm:grid-cols-2">
             <Card imgSrc={AboutOneImage} title="center" />
-            <Card imgSrc={AboutOneImage} title="center" />
+            <Card imgSrc={AboutOneImage} title="joe biden" />
             <Card imgSrc={AboutOneImage} title="center" />
             <Card imgSrc={AboutOneImage} title="center" />
           </div>
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -4,12 +4,13 @@ export default function Card({ imgSrc, title }) {
   return (
     <>
       <div
-        className="flex-1 h-full text-center content-center min-w-64"
+        className="text-center content-center"
         style={{
           backgroundImage: `url(${imgSrc})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           height: "500px",
+          minWidth: "20px",
         }}
       >
         {title}
