c970daa3fdce8c92758492c3eab86e508873a2d1
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2024-04-06 12:57:25 -0500

    about refactor

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -12,19 +12,40 @@ export default function AboutUs() {
             </h1>
           </div>
           <div className="grid grid-rows-4 grid-cols-1 justify-between gap-4  lg:grid-cols-4 lg:grid-rows-1 sm:grid-rows-2 sm:grid-cols-2">
-            <Card imgSrc={AboutOneImage} title="Comprehensive Tool Solutions" />
-            <Card imgSrc={AboutOneImage} title="Expert Fastener Distribution" />
-            <Card
-              imgSrc={AboutOneImage}
-              title="Tool Repair and Maintenance Services"
-            />
-            <Card
-              imgSrc={AboutOneImage}
-              title="Customized Tool and Fastener Consultation"
-            />
+            <Card imgSrc={AboutOneImage} title={CardInfo.CardOne.title} />
+            <Card imgSrc={AboutOneImage} title={CardInfo.CardTwo.title} />
+            <Card imgSrc={AboutOneImage} title={CardInfo.CardThree.title} />
+            <Card imgSrc={AboutOneImage} title={CardInfo.CardFour.title} />
           </div>
         </div>
       </section>
     </>
   );
 }
+
+const CardInfo = {
+  CardOne: {
+    title: "Comprehensive Tool Solutions",
+    imgSrc: { AboutOneImage },
+    description:
+      "Providing a wide range of high-quality tools and equipment for various industries, ensuring efficiency and productivity in every project.",
+  },
+  CardTwo: {
+    title: "Expert Fastener Distribution",
+    imgSrc: { AboutOneImage },
+    description:
+      "Supplying top-grade fasteners for industrial and construction needs, with a focus on reliability, durability, and superior performance in demanding environments.",
+  },
+  CardThree: {
+    title: "Tool Repair and Maintenance Services",
+    imgSrc: { AboutOneImage },
+    description:
+      "Offering professional repair and maintenance services to extend the lifespan of tools, ensuring optimal performance and cost-effectiveness for our customers.",
+  },
+  CardFour: {
+    title: "Customized Tool and Fastener Consultation",
+    imgSrc: { AboutOneImage },
+    description:
+      "Tailoring solutions to meet unique project requirements, providing expert advice, product recommendations, and personalized service for optimal tool and fastener selection.",
+  },
+};
